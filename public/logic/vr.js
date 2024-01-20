5b3b25d7251475233d0a0ba2b3b6ddf211bf41c4
oLabs <jonny.olabs@gmail.com>
2024-01-20 15:03:23 -0500

    added 2D QR Share Page Code functionality, added VR Fullscreen Overlay

---
+++ public/logic/vr.js
@@ -0,0 +1,34 @@
+console.log('HIIII!!!!')
+
+
+const
+    screenshotButtonEqui = document.getElementById('screenshotButtonEqui'),
+    screenshotButtonRect = document.getElementById('screenshotButtonRect'),
+    sharePageQr = document.getElementById('sharePageQr'),
+    fullscreenImage = document.getElementById('fullscreenImage')
+
+
+// screenshotButtonEqui.addEventListener('click', takeScreenshot('equirectangular'))
+// screenshotButtonRect.addEventListener('click', takeScreenshot('perspective'))
+sharePageQr.addEventListener('click', e => {
+    const sharePageQrImage = sharePageQr.querySelector('img')
+    const sharePageQrLink = sharePageQrImage.getAttribute('src')
+    const fullscreenImageImg = fullscreenImage.querySelector('img')
+    fullscreenImageImg.setAttribute('src', sharePageQrLink)
+    fullscreenImage.style.display = 'flex'
+})
+fullscreenImage.addEventListener('click', e => {
+    fullscreenImage.style.display = 'none'
+})
+
+// function takeScreenshot(imageType) {
+
+//     document.querySelector('a-scene').components.screenshot.capture('equirectangular')
+// }
+
+console.log(document.querySelector('a-scene').components)
+// ICON ATTRIBUTIONS
+
+// <a href="https://www.freepik.com/icon/360_1614527">Icon by Kiranshastry</a>
+
+// https://www.freepik.com/author/kiranshastry/icons/kiranshastry-gradient_630?t=f
